e8ca021c09ae2a484119c5278e6a5ea784675231
htcbtl <htcbtlsvrn.257@gmail.com>
2021-07-29 20:49:26 +0300

    added authentication db context class

---
+++ src/Emlak.Veri/DatabaseContexts/AuthenticationDbContext/AutenticationDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+
+namespace Emlak.Veri.DatabaseContexts.AutenticationDbContext
+{
+
+    public class AutenticationDbContext : IdentityDbContext
+    {
+         
+    }
+}
